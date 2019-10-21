82a07c74756a3335bca63a1550362f7c948143b7
mariaabi82 <maria.abi1982@gmail.com>
2019-10-21 20:32:52 -0300

    subo archivos

---
+++ operadores2-ejercicios.js
@@ -0,0 +1,34 @@
+// Ejercicio 34
+
+let ingresos = 1000;
+
+let ganancias = 600;
+
+let impuestos = 500;
+
+
+console.log("Objetivo de ingresos:" + (ingresos >= 800));
+
+console.log("Objetivo de impuestos: " + (impuestos < 400));
+
+console.log("Bono de ganancias: " + (ganancias == 600));
+
+// Ejercicio 36
+
+let numero1 = 10;
+
+let numero2 = 20;
+
+console.log("La variable numero1 y numero2 tienen el mismo valor: " + (numero1 == numero2));
+
+console.log("La variable numero1 y numero2 tienen el mismo valor: " + (numero1 != numero2));
+
+// Ejercicio 37
+
+let promedioParaAprobar = 6;
+
+let promedioDelAlumno = 4;
+
+console.log("El alumno tiene un promedio mayor o igual que el requerido: " + (promedioDelAlumno >= promedioParaAprobar));
+
+console.log ("Al alumno le faltaron " + (promedioParaAprobar - promedioDelAlumno) + " puntos para aprobar")
