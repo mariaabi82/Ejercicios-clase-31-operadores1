70bd473a75d3f570656b93b4a7f0e50f0218a814
mariaabi82 <maria.abi1982@gmail.com>
2019-10-23 13:06:28 -0300

    subo ejercicios del viernes

---
+++ operadores1-ejercicios.js
@@ -0,0 +1,74 @@
+// Ejercicio 27
+
+let numero1 = 4;
+
+let numero2 = 400;
+
+
+console.log(numero1 + numero2);
+
+console.log(numero1 - numero2);
+
+console.log(numero1 * numero2);
+
+console.log(numero1 / numero2);
+
+console.log(numero1 % numero2);
+
+// Ejercicio 28
+
+let numero = 9;
+
+console.log((numero*1), (numero*2), (numero*3), (numero*4), (numero*5), (numero*6), (numero*7), (numero*8), (numero*9), (numero*10));
+
+// Ejercicio 29
+
+let longitudDelLado = 10;
+
+let perimetroDelCuadrado = (longitudDelLado*4);
+
+console.log(perimetroDelCuadrado);
+
+// Ejercicio 30
+
+let longitudDeLado = 5;
+
+let areaDelCuadrado = (longitudDeLado**2);
+
+console.log(areaDelCuadrado);
+
+// Ejercicio 31
+
+let lado1 = 10;
+
+let lado2 = 20;
+
+let lado3 = 5;
+
+let perimetroDelTriangulo = (lado1+lado2+lado3);
+
+console.log(perimetroDelTriangulo)
+
+// Ejercicio 32
+
+let altura = 10;
+
+let base = 4;
+
+let areaDelTriangulo = (altura*base);
+
+// Ejercicio 33
+
+let cantidadDePersonas = 100;
+
+cantidadDePersonas +=5;
+
+console.log("Cantidad de personas: " + cantidadDePersonas);
+
+cantidadDePersonas -=3;
+
+console.log("Cantidad de personas: " + cantidadDePersonas);
+
+cantidadDePersonas *=2;
+
+console.log("Cantidad de personas: " + cantidadDePersonas);
